2e1e48fd43b05af3397e1d5b3a611b631d4042c9
Andrey Mazurov <mazurovandre@gmail.com>
2024-10-23 15:42:16 +0300

    Time: 162 ms (95.31%) | Memory: 76.9 MB (63.78%) - LeetSync

--- 883-car-fleet/car-fleet.js
+++ 883-car-fleet/car-fleet.js
@@ -5,22 +5,22 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
+    let maxTime = 0;
     const fleets = [];
-        let maxTime = 0;
-        const cars = position
-            .map((_, i) => ([position[i], speed[i], (target - position[i]) / speed[i]]))
-            .sort((a, b) => a[0] - b[0]);
+    const cars = position
+        .map((_, i) => ([position[i], speed[i], (target - position[i]) / speed[i]]))
+        .sort((a, b) => a[0] - b[0]);
 
-        for (let i = cars.length - 1; i >= 0; i--) {
-            const currentTime = cars[i][2];
+    for (let i = cars.length - 1; i >= 0; i--) {
+        const currentTime = cars[i][2];
 
-            if (currentTime > maxTime) {
-                fleets.push(currentTime);
-                maxTime = currentTime;
-            } else {
-                maxTime = Math.max(currentTime, maxTime);
-            }
+        if (currentTime > maxTime) {
+            fleets.push(currentTime);
+            maxTime = currentTime;
+        } else {
+            maxTime = Math.max(currentTime, maxTime);
         }
+    }
 
-        return fleets.length;
+    return fleets.length;
 };
