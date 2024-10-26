fff47ac6b4d7081d5043c1e0dee3a3bc43bd9987
Andrey Mazurov <mazurovandre@gmail.com>
2024-10-27 00:10:36 +0300

    Time: 15 ms (30.18%) | Memory: 68.2 MB (12.30%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const prefix = new Array(nums.length).fill(1);
+    const postfix = new Array(nums.length).fill(1);
+    const result = new Array(nums.length);
+
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+
+    for (let i = nums.length - 2; i >= 0; i--) {
+        postfix[i] = postfix[i + 1] * nums[i + 1];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = prefix[i] * postfix[i];
+    }
+
+    return result;
+};
