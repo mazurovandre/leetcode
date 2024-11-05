60dfacc4c73ff9fe519b518d89c5f5300202a46a
Andrey Mazurov <mazurovandre@gmail.com>
2024-11-05 21:50:32 +0300

    Time: 8 ms (95.33%) | Memory: 52.6 MB (60.99%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    if (s1.length > s2.length) {
+        return false;
+    } 
+
+    let neededChar = {};
+    let left = 0;
+    let right = 0;
+    let requiredLength = s1.length;
+    
+    for (let i = 0; i < s1.length; i++) {
+        neededChar[s1[i]] = (neededChar[s1[i]] || 0) + 1;
+    }
+    
+    while (right < s2.length) {
+        if (neededChar[s2[right]] > 0) {
+            requiredLength--;
+        } 
+
+        neededChar[s2[right]]--;
+        right++;
+
+        if (requiredLength === 0) {
+            return true;
+        } 
+
+        if (right - left === s1.length) {
+            if (neededChar[s2[left]] >= 0) {
+                requiredLength++;
+            } 
+            
+            neededChar[s2[left]]++;
+            left++
+        }
+    }
+    
+    return false;
+};
