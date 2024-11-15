c5120d6bf85211beeba2335ef68d127a4b6c0317
Andrey Mazurov <mazurovandre@gmail.com>
2024-11-15 20:40:53 +0300

    Time: 1 ms (96.96%) | Memory: 51.5 MB (85.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,16 +4,16 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    return nums1.map(num => {
-        const index = nums2.findIndex(item => item === num);
-        const slice = nums2.slice(index);
+    const map = {};
+	const stack = [];
+    
+	nums2.forEach(num => {
+		while (stack.length > 0 && stack[stack.length - 1] < num) {
+			map[stack.pop()] = num;
+		}
 
-        for (const next of slice) {
-            if (next > num) {
-                return next;
-            }
-        }
-
-        return -1;
-    })
+		stack.push(num);
+	});
+	
+	return nums1.map(num => map[num] ?? -1);
 };
