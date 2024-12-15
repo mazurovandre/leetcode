75b0d8dc2fb0e526207044a12499239ee6bd9c9f
Andrey Mazurov <mazurovandre@gmail.com>
2024-12-15 17:55:02 +0300

    Time: 1 ms (48.63%) | Memory: 55.5 MB (33.45%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+const kthSmallest = function(root, k) {
+    const result = [];
+
+    const addValue = (node) => {
+        if (!node) return;
+
+        addValue(node.left);
+        result.push(node.val);
+        addValue(node.right);
+    }
+
+    addValue(root);
+
+    return result[k - 1];
+};
