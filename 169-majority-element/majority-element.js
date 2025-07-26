e3654eb7b8338378258fbcc7a9901d640641f480
Andrey Mazurov <mazurovandre@gmail.com>
2025-07-26 10:50:26 +0300

    Time: 6 ms (36.56%) | Memory: 57.5 MB (43.34%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,17 +3,12 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const map = {};
-
-    for (const num of nums) {
-        map[num] = map[num] ? map[num] + 1 : 1;
-    }
-
-    for (const [num, amount] of Object.entries(map)) {
-        if (+amount > nums.length / 2) {
-            return +num;
+    const dict = {};
+    
+    for (let num of nums) {
+        dict[num] = (dict[num] || 0) + 1;
+        if (dict[num] > nums.length / 2) {
+            return num;
         }
     }
-
-    return 0;
 };
