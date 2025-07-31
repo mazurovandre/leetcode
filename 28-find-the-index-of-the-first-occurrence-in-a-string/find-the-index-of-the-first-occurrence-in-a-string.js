aae9e15064f5c02afb8220c233d0364e9944e09d
Andrey Mazurov <mazurovandre@gmail.com>
2025-07-31 08:07:56 +0300

    Time: 0 ms (100.00%) | Memory: 54.4 MB (26.20%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,17 +4,15 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if (haystack === needle) return 0;
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let j = 0;
 
-    for (let i = 0; i < haystack.length; i++) {
-        for (let j = 0; j < needle.length; j++) {
-            if (needle[j] !== haystack[i + j]) {
-                break;
-            }
-
-            if (j === needle.length - 1) {
-                return i;
-            }
+        while (haystack[i + j] === needle[j] && j < needle.length) {
+            j++;
+        }
+        
+        if (j === needle.length) {
+            return i;
         }
     }
 
