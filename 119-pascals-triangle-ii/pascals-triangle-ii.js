bb85a765a31854549873cecf32e715bbd1693b0f
Andrey Mazurov <mazurovandre@gmail.com>
2025-08-02 22:28:49 +0300

    Time: 0 ms (100.00%) | Memory: 53.8 MB (52.09%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -3,17 +3,14 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    if (rowIndex === 0) return [1];
-    if (rowIndex === 1) return [1, 1];
-
-    const prevRow = getRow(rowIndex - 1);
     const arr = Array(rowIndex + 1).fill(1);
 
-    for (let i = 1; i < arr.length - 1; i++) {
-        const left = prevRow[i - 1];
-        const right = prevRow[i];
+    if (rowIndex === 0) return arr;
 
-        arr[i] = left + right;
+    const prevRow = getRow(rowIndex - 1);
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        arr[i] = prevRow[i - 1] + prevRow[i];
     }
 
     return arr;
