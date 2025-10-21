bd76f67fd462b9bd7c560d3e81229fb88b2df75f
Andrey Mazurov <mazurovandre@gmail.com>
2025-10-21 10:22:40 +0300

    Time: 11 ms (10.53%) | Memory: 100.2 MB (5.03%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    let result = Infinity;
+    if (!root) return 0;
+
+    const dfs = (node, level) => {
+        if (!node) return;
+
+        if (!node.left && !node.right) {
+            result = Math.min(result, level);
+        }
+
+        dfs(node.left, level + 1);
+        dfs(node.right, level + 1);
+    }
+
+    dfs(root, 1);
+
+    return result;
+};
