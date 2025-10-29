b367bdfae6dfb09aa68939ef44d4366c7581368d
Andrey Mazurov <mazurovandre@gmail.com>
2025-10-29 23:16:34 +0300

    Time: 1 ms (96.33%) | Memory: 56.1 MB (88.75%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.js
+++ 3778-transform-array-by-parity/transform-array-by-parity.js
@@ -2,6 +2,4 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var transformArray = function(nums) {
-    return nums.map(num => num % 2 === 0 ? 0 : 1).sort();
-};
+const transformArray = nums => nums.map(num => num % 2).sort();
