e3f37820bb15e24b998e280b06a072d30943427a
Andrey Mazurov <mazurovandre@gmail.com>
2025-11-06 14:12:15 +0300

    Time: 222 ms (9.38%) | Memory: 65.8 MB (9.38%) - LeetSync

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var validStrings = function(n) {
+    const result = [];
+
+    const handle = (prev, str) => {
+        console.log('==>', prev, str)
+        if (str.length === n) {
+            result.push(str);
+            return;
+        }
+        str = '1' + str;
+        handle(1, str);
+        str = str.slice(1);
+        if (prev === 1) {
+            str = '0' + str;
+            handle(0, str);
+            str = str.slice(1);
+        }
+    }
+
+    handle(0, '0');
+    handle(1, '1');
+
+    return result;
+};
