1ff4fb6fa580abb08cc4697feee359b879f94146
Andrey Mazurov <mazurovandre@gmail.com>
2025-11-06 14:12:37 +0300

    Time: 49 ms (92.19%) | Memory: 59.8 MB (34.38%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.js
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.js
@@ -6,14 +6,15 @@ var validStrings = function(n) {
     const result = [];
 
     const handle = (prev, str) => {
-        console.log('==>', prev, str)
         if (str.length === n) {
             result.push(str);
             return;
         }
+
         str = '1' + str;
         handle(1, str);
         str = str.slice(1);
+        
         if (prev === 1) {
             str = '0' + str;
             handle(0, str);
