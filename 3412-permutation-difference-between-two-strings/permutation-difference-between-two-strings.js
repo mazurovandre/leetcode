f95240aef6d8a7f8d17536984604e1ae9c90e52c
Andrey Mazurov <mazurovandre@gmail.com>
2025-11-13 08:07:03 +0300

    Time: 5 ms (28.00%) | Memory: 58.4 MB (18.29%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.js
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.js
@@ -4,20 +4,15 @@
  * @return {number}
  */
 var findPermutationDifference = function(s, t) {
-    const countS = {};
-    const countT = {};
+    const map = {};
     let result = 0;
 
     for (let i = 0; i < s.length; i++) {
-        countS[s[i]] = i;
+        map[s[i]] = i;
     }
 
     for (let i = 0; i < t.length; i++) {
-        countT[t[i]] = i;
-    }
-
-    for (const char of s) {
-        result += Math.abs(countS[char] - countT[char]);
+        result += Math.abs(map[t[i]] - i);
     }
 
     return result;
