ce253bf3167b5558639232f4a9af6a2c02c2dfe4
Andrey Mazurov <mazurovandre@gmail.com>
2025-12-25 10:17:10 +0300

    Time: 0 ms (100.00%) | Memory: 55.7 MB (49.50%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    return nums.reduce((acc, num) => num < k ? acc + 1 : acc, 0);
+};
